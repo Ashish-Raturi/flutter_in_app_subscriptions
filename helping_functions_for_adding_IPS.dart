450ee158eaba18d572413fa3090d0c2269767791
Ashish-Raturi <ashishraturi368@gmail.com>
2022-04-19 23:44:56 +0530

    code_updated

---
+++ helping_functions_for_adding_IPS.dart
@@ -0,0 +1,341 @@
+import 'package:flutter/material.dart';
+//------- Copy 01 -------//
+import 'dart:async';
+import 'dart:io';
+import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:in_app_purchase_android/billing_client_wrappers.dart';
+import 'package:in_app_purchase_android/in_app_purchase_android.dart';
+import 'package:in_app_purchase_storekit/in_app_purchase_storekit.dart';
+import 'package:in_app_purchase_storekit/store_kit_wrappers.dart';
+//------- Copy 01 End -------//
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+//------- Copy 02 -------//
+
+//Subscription 01
+String sub1Id = Platform.isAndroid ? 'sliver_subscription' : 'your_ios_sub1_id';
+
+//Subscription 02
+String sub2Id = Platform.isAndroid ? 'gold_subscription' : 'your_ios_sub2_id';
+
+List<String> _subcriptionProductIds = <String>[
+  sub1Id,
+  sub2Id,
+];
+//------- Copy 02 End-------//
+
+class _HomepageState extends State<Homepage> {
+  String activeSubId = '';
+
+  //------- Copy 03 -------//
+  final InAppPurchase _inAppPurchase = InAppPurchase.instance;
+  late StreamSubscription<List<PurchaseDetails>> _subscription;
+  PurchaseDetails? _activeSubscriptionPurchasesDetails;
+
+  ///-------Use this Fields---------///
+  List<String> _notFoundIds = <String>[];
+  List<ProductDetails> _products = <ProductDetails>[];
+  bool _isAvailable = false;
+  String? _queryProductError;
+  bool _purchasePending = false;
+  bool _loading = true;
+
+  ///-------------------------------///
+
+  @override
+  void initState() {
+    final Stream<List<PurchaseDetails>> purchaseUpdated =
+        _inAppPurchase.purchaseStream;
+    _subscription =
+        purchaseUpdated.listen((List<PurchaseDetails> purchaseDetailsList) {
+      _listenToPurchaseUpdated(purchaseDetailsList);
+    }, onDone: () {
+      _subscription.cancel();
+    }, onError: (Object error) {
+      // handle error here.
+    });
+    initStoreInfo();
+    super.initState();
+  }
+
+  Future<void> initStoreInfo() async {
+    final bool isAvailable = await _inAppPurchase.isAvailable();
+    if (!isAvailable) {
+      setState(() {
+        _isAvailable = isAvailable;
+        _products = <ProductDetails>[];
+
+        _notFoundIds = <String>[];
+
+        _purchasePending = false;
+        _loading = false;
+      });
+      return;
+    }
+
+    if (Platform.isIOS) {
+      final InAppPurchaseStoreKitPlatformAddition iosPlatformAddition =
+          _inAppPurchase
+              .getPlatformAddition<InAppPurchaseStoreKitPlatformAddition>();
+      await iosPlatformAddition.setDelegate(ExamplePaymentQueueDelegate());
+    }
+
+    final ProductDetailsResponse productDetailResponse = await _inAppPurchase
+        .queryProductDetails(_subcriptionProductIds.toSet());
+    if (productDetailResponse.error != null) {
+      setState(() {
+        _queryProductError = productDetailResponse.error!.message;
+        _isAvailable = isAvailable;
+        _products = productDetailResponse.productDetails;
+
+        _notFoundIds = productDetailResponse.notFoundIDs;
+
+        _purchasePending = false;
+        _loading = false;
+      });
+      return;
+    }
+
+    if (productDetailResponse.productDetails.isEmpty) {
+      setState(() {
+        _queryProductError = null;
+        _isAvailable = isAvailable;
+        _products = productDetailResponse.productDetails;
+
+        _notFoundIds = productDetailResponse.notFoundIDs;
+
+        _purchasePending = false;
+        _loading = false;
+      });
+      return;
+    }
+
+    setState(() {
+      _isAvailable = isAvailable;
+      _products = productDetailResponse.productDetails;
+      _notFoundIds = productDetailResponse.notFoundIDs;
+
+      _purchasePending = false;
+      _loading = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    if (Platform.isIOS) {
+      final InAppPurchaseStoreKitPlatformAddition iosPlatformAddition =
+          _inAppPurchase
+              .getPlatformAddition<InAppPurchaseStoreKitPlatformAddition>();
+      iosPlatformAddition.setDelegate(null);
+    }
+    _subscription.cancel();
+    super.dispose();
+  }
+
+  //------- Copy 03 End -------//
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  //------- Copy 04 -------//
+
+  Card _buildConnectionCheckTile() {
+    if (_loading) {
+      return const Card(child: ListTile(title: Text('Trying to connect...')));
+    }
+    final Widget storeHeader = ListTile(
+      leading: Icon(_isAvailable ? Icons.check : Icons.block,
+          color: _isAvailable ? Colors.green : ThemeData.light().errorColor),
+      title: Text(
+          'The store is ' + (_isAvailable ? 'available' : 'unavailable') + '.'),
+    );
+    final List<Widget> children = <Widget>[storeHeader];
+
+    if (!_isAvailable) {
+      children.addAll(<Widget>[
+        const Divider(),
+        ListTile(
+          title: Text('Not connected',
+              style: TextStyle(color: ThemeData.light().errorColor)),
+          subtitle: const Text(
+              'Unable to connect to the payments processor. Has this app been configured correctly? See the example README for instructions.'),
+        ),
+      ]);
+    }
+    return Card(child: Column(children: children));
+  }
+
+  Widget _buildRestoreButton() {
+    if (_loading) {
+      return Container();
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          TextButton(
+            child: const Text('Restore purchases'),
+            style: TextButton.styleFrom(
+              backgroundColor: Theme.of(context).primaryColor,
+              primary: Colors.white,
+            ),
+            onPressed: () => _inAppPurchase.restorePurchases(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void showPendingUI() {
+    setState(() {
+      _purchasePending = true;
+    });
+  }
+
+  ProductDetails? findProductDetail(String id) {
+    for (ProductDetails pd in _products) {
+      if (pd.id == id) return pd;
+    }
+    return null;
+  }
+
+  buySubscription(ProductDetails productDetails) {
+    late PurchaseParam purchaseParam;
+
+    if (Platform.isAndroid) {
+      final GooglePlayPurchaseDetails? oldSubscription = _getOldSubscription(
+          productDetails, _activeSubscriptionPurchasesDetails);
+
+      purchaseParam = GooglePlayPurchaseParam(
+          productDetails: productDetails,
+          applicationUserName: null,
+          changeSubscriptionParam: (oldSubscription != null)
+              ? ChangeSubscriptionParam(
+                  oldPurchaseDetails: oldSubscription,
+                  prorationMode: ProrationMode.immediateAndChargeProratedPrice,
+                )
+              : null);
+    } else {
+      purchaseParam = PurchaseParam(
+        productDetails: productDetails,
+        applicationUserName: null,
+      );
+    }
+    //buying Subscription
+    _inAppPurchase.buyNonConsumable(purchaseParam: purchaseParam);
+  }
+
+  Future<void> verifyAndDeliverProduct(PurchaseDetails purchaseDetails) async {
+    //Verify Purchase
+    //Deliver Product
+    setState(() {
+      _activeSubscriptionPurchasesDetails = purchaseDetails;
+      activeSubId = purchaseDetails.productID;
+      _purchasePending = false;
+    });
+  }
+
+  void handleError(IAPError error) {
+    setState(() {
+      _purchasePending = false;
+    });
+  }
+
+  Future<void> _listenToPurchaseUpdated(
+      List<PurchaseDetails> purchaseDetailsList) async {
+    for (final PurchaseDetails purchaseDetails in purchaseDetailsList) {
+      if (purchaseDetails.status == PurchaseStatus.pending) {
+        showPendingUI();
+      } else {
+        if (purchaseDetails.status == PurchaseStatus.error) {
+          handleError(purchaseDetails.error!);
+        } else if (purchaseDetails.status == PurchaseStatus.purchased ||
+            purchaseDetails.status == PurchaseStatus.restored) {
+          verifyAndDeliverProduct(purchaseDetails);
+        }
+        if (purchaseDetails.pendingCompletePurchase) {
+          await _inAppPurchase.completePurchase(purchaseDetails);
+        }
+      }
+    }
+  }
+
+  Future<void> confirmPriceChange(
+      BuildContext context, String purchaseId) async {
+    if (Platform.isAndroid) {
+      final InAppPurchaseAndroidPlatformAddition androidAddition =
+          _inAppPurchase
+              .getPlatformAddition<InAppPurchaseAndroidPlatformAddition>();
+      final BillingResultWrapper priceChangeConfirmationResult =
+          await androidAddition.launchPriceChangeConfirmationFlow(
+        sku: purchaseId,
+      );
+      if (priceChangeConfirmationResult.responseCode == BillingResponse.ok) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Price change accepted'),
+        ));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+            priceChangeConfirmationResult.debugMessage ??
+                'Price change failed with code ${priceChangeConfirmationResult.responseCode}',
+          ),
+        ));
+      }
+    }
+    if (Platform.isIOS) {
+      final InAppPurchaseStoreKitPlatformAddition iapStoreKitPlatformAddition =
+          _inAppPurchase
+              .getPlatformAddition<InAppPurchaseStoreKitPlatformAddition>();
+      await iapStoreKitPlatformAddition.showPriceConsentIfNeeded();
+    }
+  }
+
+  GooglePlayPurchaseDetails? _getOldSubscription(
+      ProductDetails productDetails, PurchaseDetails? purchase) {
+    GooglePlayPurchaseDetails? oldSubscription;
+    if (productDetails.id == sub1Id &&
+        purchase != null &&
+        purchase.productID == sub2Id) {
+      oldSubscription = purchase as GooglePlayPurchaseDetails;
+    } else if (productDetails.id == sub2Id &&
+        purchase != null &&
+        purchase.productID == sub1Id) {
+      oldSubscription = purchase as GooglePlayPurchaseDetails;
+    }
+    return oldSubscription;
+  }
+  //------- Copy 04 End -------//
+}
+
+//------- Copy 05 -------//
+
+/// Example implementation of the
+/// [`SKPaymentQueueDelegate`](https://developer.apple.com/documentation/storekit/skpaymentqueuedelegate?language=objc).
+///
+/// The payment queue delegate can be implementated to provide information
+/// needed to complete transactions.s
+class ExamplePaymentQueueDelegate implements SKPaymentQueueDelegateWrapper {
+  @override
+  bool shouldContinueTransaction(
+      SKPaymentTransactionWrapper transaction, SKStorefrontWrapper storefront) {
+    return true;
+  }
+
+  @override
+  bool shouldShowPriceConsent() {
+    return false;
+  }
+}
+//------- Copy 05 End-------//
