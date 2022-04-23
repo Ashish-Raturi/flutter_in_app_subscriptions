123e8d1e5249a5b69a1b77c715797453c8b8d81b
Ashish-Raturi <ashishraturi368@gmail.com>
2022-04-23 23:10:14 +0530

    code_updated

--- step_02/app/lib/home_page.dart
+++ step_02/app/lib/home_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:pim/color.dart';
+import 'package:pim/service/subscription_db_service.dart';
+import 'dart:io';
+import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:in_app_purchase_storekit/in_app_purchase_storekit.dart';
+import 'package:in_app_purchase_storekit/store_kit_wrappers.dart';
+import 'package:in_app_purchase_android/billing_client_wrappers.dart';
+import 'package:in_app_purchase_android/in_app_purchase_android.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -9,69 +15,221 @@ class Homepage extends StatefulWidget {
   _HomepageState createState() => _HomepageState();
 }
 
-String sub1Id = "starter plan";
-String sub2Id = "pro plan";
+//Subscription 01
+String sub1Id =
+    Platform.isAndroid ? 'monthly_subscription' : 'your_ios_sub1_id';
+
+//Subscription 02
+String sub2Id = Platform.isAndroid ? 'yearly_subscription' : 'your_ios_sub2_id';
+
+List<String> _subcriptionProductIds = <String>[
+  sub1Id,
+  sub2Id,
+];
 
 class _HomepageState extends State<Homepage> {
-  String activeSubId = '';
+  String activeSubId = "";
 
   final InAppPurchase _inAppPurchase = InAppPurchase.instance;
-  bool _loading = true;
-
-  ///step 01 case 01
-  bool _isAvailable = false;
-
-  ///step 01 case 02
   List<ProductDetails> _products = <ProductDetails>[];
   List<String> _notFoundIds = <String>[];
+  bool _isAvailable = false;
+  String? _queryProductError;
+  bool _loading = true;
+
+  @override
+  void initState() {
+    initStoreInfo();
+    super.initState();
+  }
+
+  Future<void> initStoreInfo() async {
+    final bool isAvailable = await _inAppPurchase.isAvailable();
+    if (!isAvailable) {
+      setState(() {
+        _isAvailable = isAvailable;
+        _products = <ProductDetails>[];
+        _notFoundIds = <String>[];
+        _loading = false;
+      });
+      return;
+    }
+
+    if (Platform.isIOS) {
+      final InAppPurchaseStoreKitPlatformAddition iosPlatformAddition =
+          _inAppPurchase
+              .getPlatformAddition<InAppPurchaseStoreKitPlatformAddition>();
+      await iosPlatformAddition.setDelegate(ExamplePaymentQueueDelegate());
+    }
+
+    final ProductDetailsResponse productDetailResponse = await _inAppPurchase
+        .queryProductDetails(_subcriptionProductIds.toSet());
+    if (productDetailResponse.error != null) {
+      setState(() {
+        _queryProductError = productDetailResponse.error!.message;
+        _isAvailable = isAvailable;
+        _products = productDetailResponse.productDetails;
+        _notFoundIds = productDetailResponse.notFoundIDs;
+        _loading = false;
+      });
+      return;
+    }
+
+    if (productDetailResponse.productDetails.isEmpty) {
+      setState(() {
+        _queryProductError = null;
+        _isAvailable = isAvailable;
+        _products = productDetailResponse.productDetails;
+        _notFoundIds = productDetailResponse.notFoundIDs;
+        _loading = false;
+      });
+      return;
+    }
+
+    setState(() {
+      _isAvailable = isAvailable;
+      _products = productDetailResponse.productDetails;
+      _notFoundIds = productDetailResponse.notFoundIDs;
+      _loading = false;
+    });
+  }
+
+  Card _buildConnectionCheckTile() {
+    if (_loading) {
+      return const Card(child: ListTile(title: Text('Trying to connect...')));
+    }
+    final Widget storeHeader = ListTile(
+      leading: Icon(_isAvailable ? Icons.check : Icons.block,
+          color: _isAvailable ? Colors.green : ThemeData.light().errorColor),
+      title: Text(
+          'The store is ' + (_isAvailable ? 'available' : 'unavailable') + '.'),
+    );
+    final List<Widget> children = <Widget>[storeHeader];
+
+    if (!_isAvailable) {
+      children.addAll(<Widget>[
+        const Divider(),
+        ListTile(
+          title: Text('Not connected',
+              style: TextStyle(color: ThemeData.light().errorColor)),
+          subtitle: const Text(
+              'Unable to connect to the payments processor. Has this app been configured correctly? See the example README for instructions.'),
+        ),
+      ]);
+    }
+    return Card(child: Column(children: children));
+  }
+
+  ProductDetails? findProductDetail(String id) {
+    for (ProductDetails pd in _products) {
+      if (pd.id == id) return pd;
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-          backgroundColor: Colors.white,
-          body: Stack(
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('Hi, Ashish',
-                              overflow: TextOverflow.ellipsis,
-                              style: const TextStyle(
-                                  fontSize: 22,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold)),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      _buildMonthlySubTile(),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      _buildYearlySubTile(),
-                    ],
-                  ),
+    return StreamBuilder<UserData>(
+        stream: SubscriptionDbService().featchUserDataFromDb,
+        builder: (context, snapshot) {
+          if (snapshot.hasData == false) {
+            return Scaffold(
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    CircularProgressIndicator(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text('Loading User Data, Please wait...')
+                  ],
                 ),
               ),
-            ],
-          )),
-    );
+            );
+          }
+
+          UserData userData = snapshot.data!;
+
+          return SafeArea(
+            child: Scaffold(
+                backgroundColor: Colors.white,
+                body: _loading
+                    ? Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            CircularProgressIndicator(),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text('Loading Product Details, Please wait...')
+                          ],
+                        ),
+                      )
+                    : Stack(
+                        children: [
+                          if (_queryProductError != null)
+                            Center(
+                                child: Padding(
+                              padding: const EdgeInsets.all(18.0),
+                              child: Text(
+                                'Query Product Error(Got Error While Fetching Product Details) : $_queryProductError',
+                                textAlign: TextAlign.center,
+                              ),
+                            )),
+                          if (_queryProductError == null)
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 5),
+                              child: SingleChildScrollView(
+                                child: Column(
+                                  children: [
+                                    const SizedBox(
+                                      height: 20,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text('Hi, ${userData.username}',
+                                            overflow: TextOverflow.ellipsis,
+                                            style: const TextStyle(
+                                                fontSize: 22,
+                                                color: Colors.black,
+                                                fontWeight: FontWeight.bold)),
+                                        _buildRestoreButton()
+                                      ],
+                                    ),
+                                    _buildConnectionCheckTile(),
+                                    const SizedBox(
+                                      height: 20,
+                                    ),
+                                    if (!_notFoundIds.contains(sub1Id) &&
+                                        _isAvailable)
+                                      _buildMonthlySubTile(),
+                                    const SizedBox(
+                                      height: 20,
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    if (!_notFoundIds.contains(sub2Id) &&
+                                        _isAvailable)
+                                      _buildYearlySubTile(),
+                                  ],
+                                ),
+                              ),
+                            ),
+                        ],
+                      )),
+          );
+        });
   }
 
   _buildMonthlySubTile() {
+    ProductDetails pd = findProductDetail(sub1Id)!;
     return Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(14),
@@ -86,7 +244,8 @@ class _HomepageState extends State<Homepage> {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Product title',
+                    Text(pd.title,
+                        // 'Product title',
                         style: const TextStyle(
                             fontWeight: FontWeight.w500,
                             color: Colors.white,
@@ -94,7 +253,8 @@ class _HomepageState extends State<Homepage> {
                     const SizedBox(
                       height: 5,
                     ),
-                    Text('Price',
+                    Text(pd.price,
+                        // 'Price',
                         style: const TextStyle(
                             fontSize: 30,
                             fontWeight: FontWeight.w500,
@@ -134,14 +294,16 @@ class _HomepageState extends State<Homepage> {
             Divider(),
             Align(
               alignment: Alignment.centerLeft,
-              child: Text('Description : product description',
+              child: Text('Description : ${pd.description}',
                   style: TextStyle(color: c3, fontSize: 16)),
             ),
             const SizedBox(
               height: 20,
             ),
             GestureDetector(
-              onTap: () {},
+              onTap: () {
+                buySubscription(pd);
+              },
               child: Container(
                 padding: const EdgeInsets.symmetric(vertical: 20),
                 decoration: BoxDecoration(
@@ -167,6 +329,7 @@ class _HomepageState extends State<Homepage> {
   }
 
   _buildYearlySubTile() {
+    ProductDetails pd = findProductDetail(sub2Id)!;
     return Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(14),
@@ -181,7 +344,8 @@ class _HomepageState extends State<Homepage> {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Product title',
+                    Text(pd.title,
+                        // 'Product title',
                         style: const TextStyle(
                             fontWeight: FontWeight.w500,
                             color: Colors.white,
@@ -189,7 +353,8 @@ class _HomepageState extends State<Homepage> {
                     const SizedBox(
                       height: 5,
                     ),
-                    Text('Price',
+                    Text(pd.price,
+                        // 'Price',
                         style: const TextStyle(
                             fontSize: 30,
                             fontWeight: FontWeight.w500,
@@ -229,14 +394,16 @@ class _HomepageState extends State<Homepage> {
             Divider(),
             Align(
               alignment: Alignment.centerLeft,
-              child: Text('Description : product description',
+              child: Text('Description : ${pd.description}',
                   style: TextStyle(color: c3, fontSize: 16)),
             ),
             const SizedBox(
               height: 20,
             ),
             GestureDetector(
-              onTap: () {},
+              onTap: () {
+                buySubscription(pd);
+              },
               child: Container(
                 padding: const EdgeInsets.symmetric(vertical: 20),
                 decoration: BoxDecoration(
@@ -259,5 +426,104 @@ class _HomepageState extends State<Homepage> {
             ),
           ],
         ));
+  }
+
+  buySubscription(ProductDetails productDetails) {
+    late PurchaseParam purchaseParam;
+
+    if (Platform.isAndroid) {
+      //updated oldSubscription details for upgrading and downgrading subscription
+      GooglePlayPurchaseDetails? oldSubscription;
+
+      purchaseParam = GooglePlayPurchaseParam(
+          productDetails: productDetails,
+          applicationUserName: null,
+          changeSubscriptionParam: (oldSubscription != null)
+              ? ChangeSubscriptionParam(
+                  oldPurchaseDetails: oldSubscription,
+                  prorationMode: ProrationMode.immediateAndChargeProratedPrice,
+                )
+              : null);
+    } else {
+      purchaseParam = PurchaseParam(
+        productDetails: productDetails,
+        applicationUserName: null,
+      );
+    }
+    //buying Subscription
+    _inAppPurchase.buyNonConsumable(purchaseParam: purchaseParam);
+  }
+
+  Widget _buildRestoreButton() {
+    if (_loading) {
+      return Container();
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          TextButton(
+            child: const Text('Restore purchases'),
+            style: TextButton.styleFrom(
+              backgroundColor: Theme.of(context).primaryColor,
+              primary: Colors.white,
+            ),
+            onPressed: () => _inAppPurchase.restorePurchases(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> confirmPriceChange(
+      BuildContext context, String purchaseId) async {
+    if (Platform.isAndroid) {
+      final InAppPurchaseAndroidPlatformAddition androidAddition =
+          _inAppPurchase
+              .getPlatformAddition<InAppPurchaseAndroidPlatformAddition>();
+      final BillingResultWrapper priceChangeConfirmationResult =
+          await androidAddition.launchPriceChangeConfirmationFlow(
+        sku: purchaseId,
+      );
+      if (priceChangeConfirmationResult.responseCode == BillingResponse.ok) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Price change accepted'),
+        ));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(
+            priceChangeConfirmationResult.debugMessage ??
+                'Price change failed with code ${priceChangeConfirmationResult.responseCode}',
+          ),
+        ));
+      }
+    }
+    if (Platform.isIOS) {
+      final InAppPurchaseStoreKitPlatformAddition iapStoreKitPlatformAddition =
+          _inAppPurchase
+              .getPlatformAddition<InAppPurchaseStoreKitPlatformAddition>();
+      await iapStoreKitPlatformAddition.showPriceConsentIfNeeded();
+    }
+  }
+}
+
+/// Example implementation of the
+/// [`SKPaymentQueueDelegate`](https://developer.apple.com/documentation/storekit/skpaymentqueuedelegate?language=objc).
+///
+/// The payment queue delegate can be implementated to provide information
+/// needed to complete transactions.s
+class ExamplePaymentQueueDelegate implements SKPaymentQueueDelegateWrapper {
+  @override
+  bool shouldContinueTransaction(
+      SKPaymentTransactionWrapper transaction, SKStorefrontWrapper storefront) {
+    return true;
+  }
+
+  @override
+  bool shouldShowPriceConsent() {
+    return false;
   }
 }
